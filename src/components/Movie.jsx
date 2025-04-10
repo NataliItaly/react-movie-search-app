d9a3f9c2d1b824c49c13a0aafa83fb64022b0aba
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2025-04-10 15:02:17 +0200

    feat: add media styles to movies list

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,6 +4,8 @@ export default function Movie({ movie, addToWatchList }) {
   const runtime =
     movie.Runtime === "N/A" ? "runtime not available" : movie.Runtime;
   const actors = movie.Actors === "N/A" ? "not available" : movie.Actors;
+  const director = movie.Director === "N/A" ? "not available" : movie.Director;
+
   const plot =
     movie.Plot === "N/A" ? "Description is not available" : movie.Plot;
   const btnClass = movie.addedWatchList
@@ -13,37 +15,52 @@ export default function Movie({ movie, addToWatchList }) {
   return (
     <li className="movie" data-movie={movie.imdbID}>
       <img src={imgSrc} alt={movie.Title} className="movie__poster" />
-      <div className="movie__data">
+      <div className="movie__header">
         <h2 className="movie__title">{movie.Title}</h2>
-        <div className="movie__header">
-          <span className="movie__year">{movie.Year}</span>
-          <span className="movie__country">{movie.Country}</span>
-          <span className="movie__runtime">{runtime}</span>
-          <span className="movie__genre">{movie.Genre}</span>
-          <p className="movie__rate">
-            <svg
-              className="movie__rate-icon"
-              width="20"
-              height="20"
-              viewBox="0 0 30 30"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 1L19 11H29L21 18L24 29L15 22L6 29L9 18L1 11H11L15 1Z"
-                fill="#00f1ff"
-                stroke="#00f1ff"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <span className="movie__rating">{rating}</span>
-          </p>
-        </div>
-        <div className="movie__actors">
+        <p className="movie__year">
+          <span className="movie__info">Year:&nbsp;</span>
+          {movie.Year}
+        </p>
+        <p className="movie__country">
+          <span className="movie__info">Country:&nbsp;</span>
+          {movie.Country}
+        </p>
+        <p className="movie__runtime">
+          <span className="movie__info">Runtime:&nbsp;</span>
+          {runtime}
+        </p>
+        <p className="movie__genre">
+          <span className="movie__info">Genre:&nbsp;</span>
+          {movie.Genre}
+        </p>
+        <p className="movie__director">
+          <span className="movie__info">Director:</span> {director}
+        </p>
+        <div className="movie__actor">
           <span className="movie__info">Actors:</span> {actors}
         </div>
+        <p className="movie__rate">
+          <svg
+            className="movie__rate-icon"
+            width="20"
+            height="20"
+            viewBox="0 0 30 30"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15 1L19 11H29L21 18L24 29L15 22L6 29L9 18L1 11H11L15 1Z"
+              fill="#e878f9"
+              stroke="#e878f9"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <span className="movie__rating">{rating}</span>
+        </p>
+      </div>
+      <div className="movie__data">
         <div className="movie__description">
           <span className="movie__info">Description: </span>
           {plot}
